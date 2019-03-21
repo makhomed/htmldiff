705750ccfdce8d1da3ae515d04d2819b91563e88
Gena Makhomed <makhomed@gmail.com>
2019-03-21 18:04:56 +0200

    update license

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version="1.0.0.dev7",
     author="Ian Bicking - https://github.com/ianb",
     description=("Tool for diffing html files"),
+    license='MIT',
     packages=find_packages('src'),
     package_dir={"": "src"},
     install_requires=["boltons>=17.1.0"],
